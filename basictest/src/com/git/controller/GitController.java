949ac3eca035c727f159af356eaff60577b0542b
User <User@183.99.80.251>
2021-04-14 10:14:56 +0900

    #03 기능추가(testPrint,calcPlus) -bs-

---
+++ basictest/src/com/git/controller/GitController.java
@@ -0,0 +1,11 @@
+package com.git.controller;
+
+public class GitController {
+	public void testPrint() {
+		System.out.print("프린트하는것");
+	}
+	
+	public int calcPlus(int su1, int su2) {
+		return su1+su2;
+	}
+}
